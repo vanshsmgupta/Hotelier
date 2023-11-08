c1533456d8cc30d0cfcdc64862b8adb19da99750
SachinRajkumarSaini <sachinrajkumarsaini2002@gmail.com>
2023-11-09 04:44:28 +0530

    Add Email Service

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const sendMail = () => {
+  const params = {
+    name: document.getElementById("name").value,
+    number: document.getElementById("number").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
+  emailjs
+    .send("service_w4x7sz3", "template_nk1z848", params)
+    .then((response) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      alert("Message sent successfully");
+    });
+};
+
 const menuIcon = document.getElementById("menu-icon");
 const navLinks = document.getElementById("nav-links");
 
